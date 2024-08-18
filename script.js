cb3c60249744305210cd2a4bc2762cbf475f43d6
Sharmila-Rajendran <139245927+Sharmila-Rajendran@users.noreply.github.com>
2024-08-18 17:49:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const Calculate= () => {
+    let amount = document.getElementById("amount").value;
+    let interest = document.getElementById("interest").value;
+    let months = document.getElementById("months").value;
+    
+    let monthlyPayment = ((amount / months) + interest);
+    let totalInterest = (amount * (interest * 0.01)) / months;
+    let totalPayment = parseFloat(amount) + parseFloat(totalInterest);
+    
+    document.getElementById("result").innerHTML = `
+                    <h2>Results </h2>
+                    <p>Total payment : ${parseFloat(totalPayment).toFixed(2)}</p> 
+                    <p>Monthly Payment : ${parseFloat(monthlyPayment).toFixed(2)}</p>
+                    <p>Total Interest : ${parseFloat(totalInterest).toFixed(2)}</p>`;
+}
